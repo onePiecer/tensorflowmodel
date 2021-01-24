e489b24449df10810bbd3aa0e9385d2f0f1b41f1
caijian1 <caijian1@xiaomi.com>
2021-01-24 23:14:39 +0800

    MLP WideNDeep 基本模型实现

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # tensorflowmodel
+使用TensorFlow实现的算法模型
+## recommend
+推荐算法模型
---
+++ recommend/model/WideNDeep.py
@@ -0,0 +1,107 @@
+import tensorflow as tf
+
+training_samples_file_path = tf.keras.utils.get_file("trainingSamples.csv",
+                                                     "file:///C:/Users/cj\PycharmProjects/tensorflowstudy/sparrowrecsys/data/trainingSamples.csv")
+test_samples_file_path = tf.keras.utils.get_file("testSamples.csv",
+                                                     "file:///C:/Users/cj\PycharmProjects/tensorflowstudy/sparrowrecsys/data/testSamples.csv")
+
+def get_dataset(file_path):
+    dataset = tf.data.experimental.make_csv_dataset(
+        file_path,
+        batch_size=12,
+        label_name='label',
+        na_value="0",
+        num_epochs=1,
+        ignore_errors=True
+    )
+    return dataset
+
+train_dataset = get_dataset(training_samples_file_path)
+test_dataset = get_dataset(test_samples_file_path)
+
+genre_vocab = ['Film-Noir', 'Action', 'Adventure', 'Horror', 'Romance', 'War', 'Comedy', 'Western', 'Documentary',
+               'Sci-Fi', 'Drama', 'Thriller',
+               'Crime', 'Fantasy', 'Animation', 'IMAX', 'Mystery', 'Children', 'Musical']
+
+GENRE_FEATURES = {
+    'userGenre1': genre_vocab,
+    'userGenre2': genre_vocab,
+    'userGenre3': genre_vocab,
+    'userGenre4': genre_vocab,
+    'userGenre5': genre_vocab,
+    'movieGenre1': genre_vocab,
+    'movieGenre2': genre_vocab,
+    'movieGenre3': genre_vocab
+}
+
+categorical_columns = []
+for feature, vocal in GENRE_FEATURES.items():
+    cat_col = tf.feature_column.categorical_column_with_vocabulary_list(
+        key=feature, vocabulary_list=vocal
+    )
+    emb_col = tf.feature_column.embedding_column(cat_col, 10)
+    categorical_columns.append(emb_col)
+
+movie_col = tf.feature_column.categorical_column_with_identity(key='movieId', num_buckets=1001)
+movie_emb_col = tf.feature_column.embedding_column(movie_col, 10)
+categorical_columns.append(movie_emb_col)
+
+user_col = tf.feature_column.categorical_column_with_identity(key='userId', num_buckets=30001)
+user_em_col = tf.feature_column.embedding_column(user_col, 10)
+categorical_columns.append(user_em_col)
+
+numerical_columns = [
+    tf.feature_column.numeric_column('releaseYear'),
+    tf.feature_column.numeric_column('movieRatingCount'),
+    tf.feature_column.numeric_column('movieAvgRating'),
+    tf.feature_column.numeric_column('movieRatingStddev'),
+    tf.feature_column.numeric_column('userRatingCount'),
+    tf.feature_column.numeric_column('userAvgRating'),
+    tf.feature_column.numeric_column('userRatingStddev')
+]
+
+rated_movie = tf.feature_column.categorical_column_with_identity(key='userRatedMovie1', num_buckets=1001)
+cross_feature = tf.feature_column.indicator_column(tf.feature_column.crossed_column([movie_col, rated_movie], 10000))
+
+inputs = {
+    "movieAvgRating": tf.keras.layers.Input(name="movieAvgRating", shape=(), dtype='float32'),
+    'movieRatingStddev': tf.keras.layers.Input(name='movieRatingStddev', shape=(), dtype='float32'),
+    'movieRatingCount': tf.keras.layers.Input(name='movieRatingCount', shape=(), dtype='int32'),
+    'userAvgRating': tf.keras.layers.Input(name='userAvgRating', shape=(), dtype='float32'),
+    'userRatingStddev': tf.keras.layers.Input(name='userRatingStddev', shape=(), dtype='float32'),
+    'userRatingCount': tf.keras.layers.Input(name='userRatingCount', shape=(), dtype='int32'),
+    'releaseYear': tf.keras.layers.Input(name='releaseYear', shape=(), dtype='int32'),
+
+    'movieId': tf.keras.layers.Input(name='movieId', shape=(), dtype='int32'),
+    'userId': tf.keras.layers.Input(name='userId', shape=(), dtype='int32'),
+    'userRatedMovie1': tf.keras.layers.Input(name='userRatedMovie1', shape=(), dtype='int32'),
+
+    'userGenre1': tf.keras.layers.Input(name='userGenre1', shape=(), dtype='string'),
+    'userGenre2': tf.keras.layers.Input(name='userGenre2', shape=(), dtype='string'),
+    'userGenre3': tf.keras.layers.Input(name='userGenre3', shape=(), dtype='string'),
+    'userGenre4': tf.keras.layers.Input(name='userGenre4', shape=(), dtype='string'),
+    'userGenre5': tf.keras.layers.Input(name='userGenre5', shape=(), dtype='string'),
+    'movieGenre1': tf.keras.layers.Input(name='movieGenre1', shape=(), dtype='string'),
+    'movieGenre2': tf.keras.layers.Input(name='movieGenre2', shape=(), dtype='string'),
+    'movieGenre3': tf.keras.layers.Input(name='movieGenre3', shape=(), dtype='string'),
+}
+
+# wide and deep model architecture
+# deep part for all input features
+deep = tf.keras.layers.DenseFeatures(numerical_columns + categorical_columns)(inputs)
+deep = tf.keras.layers.Dense(128, activation='relu')(deep)
+deep = tf.keras.layers.Dense(128, activation='relu')(deep)
+# wide part for cross feature
+wide = tf.keras.layers.DenseFeatures(cross_feature)(inputs)
+both = tf.keras.layers.concatenate([wide, deep])
+output_layer = tf.keras.layers.Dense(1, activation='sigmoid')(both)
+model = tf.keras.Model(inputs, output_layer)
+
+# compile the model, set loss function, optimizer and evaluation metrics
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy', tf.keras.metrics.AUC(curve='ROC'), tf.keras.metrics.AUC(curve='PR')])
+model.fit(train_dataset, epochs=5)
+test_loss, test_accuracy, test_roc_auc, test_pr_auc = model.evaluate(test_dataset)
+print('\n\nTest Loss {}, Test Accuracy {}, Test ROC AUC {}, Test PR AUC {}'.format(test_loss, test_accuracy,
+                                                                                   test_roc_auc, test_pr_auc))
